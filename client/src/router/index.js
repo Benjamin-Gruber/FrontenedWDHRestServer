5ddb795855eae0a1824e25dd3b34ceceead20028
Benjamin-Gruber <83592290+Benjamin-Gruber@users.noreply.github.com>
2021-10-07 16:20:05 +0200

    Erstellen  Details.vue

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Details from '../views/Details.vue';
 
 Vue.use(VueRouter)
 
@@ -17,6 +18,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/details/:id',
+    name: Details,
+    props: true,
+    component: () => import(/* webpackChunkName: "details" */ '../views/Details.vue')
   }
 ]
 
